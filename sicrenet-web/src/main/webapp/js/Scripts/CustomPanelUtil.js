308be4ac1e263828eb7ce2ea96efb90ba3d8aca8
Said <smoralesle@elektra.com>
2017-03-23 13:36:03 -0600

    doc

--- sicrenet-web/src/main/webapp/js/Scripts/CustomPanelUtil.js
+++
@@ -1,6 +0,0 @@
-function ShowDivDialogFuente(){
-	document.getElementById("idDivDialogFuente").style.display = 'block';
-}
-function HideDivDialogFuente(){
-	document.getElementById("idDivDialogFuente").style.display = 'none';
-}
--- sicrenet-web/src/main/webapp/pages/Inicio/Principal/Respuestas.aspx
+++
@@ -1,2 +0,0 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Respuestas.aspx.cs" Inherits="SicreNet.Inicio.Principal.Respuestas" %>
- 
